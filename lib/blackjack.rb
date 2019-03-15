ed16f00ba6a61a354524c1bea0afe0ef88a7a870
Sarah Anderson <saranderson13@gmail.com>
2019-03-15 01:23:16 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -57,11 +57,11 @@ end
 
 def runner
   welcome
-  card_total = 0
   initial_round
   while card_total < 21
     hit?(card_total)
     display_card_total(card_total)
+    card_total
   end
   end_game(card_total)
 end
